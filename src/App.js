83fd93f57cbf9e5ef2e4cfe0889803ae97196772
PHUWARIN MANEERAT <6131305047@lamduan.mfu.ac.th>
2021-06-30 16:49:17 +0700

    Update UI

--- src/App.js
+++ src/App.js
@@ -4,7 +4,53 @@ import './App.css';
 import FormInput from './Form';
 import uniqueID from './uniqueID';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 700,
+    minHeight: 55,
+    marginBottom: 10,
+    marginTop: 10,
+    marginRight: 10
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 20,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  button: {
+    marginLeft: 20,
+    marginTop: 10,
+  },
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridGap: theme.spacing(3),
+  },
+  paper: {
+    marginTop: 7,
+    marginLeft: 10,
+    fontSize: 20,
+    padding: theme.spacing(1),
+    backgroundColor: "#e88073",
+  },
+
+}));
+
 const App = () => {
+
+  const classes = useStyles();
 
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
@@ -36,6 +82,7 @@ const App = () => {
   }
 
 
+
   return (
     <div className="App">
       <center>
@@ -45,12 +92,14 @@ const App = () => {
           <input
             type="text"
             name="todo"
-            placeholder="Fill your activity"
+            placeholder="    Fill your activity"
             value={todo}
             onChange={handleInputChange}
           />
           {" "}
-          <button type="submit">Add</button>
+          <Button className="addBut" type="submit" variant="contained" style={{ backgroundColor: "#e6e6e6" }}>
+            ADD
+          </Button>
           <br />
         </form>
           {todos.map((item) => {
@@ -62,9 +111,25 @@ const App = () => {
                 setTodos={setTodos}
               />) :
               (<div key={item.id}>
-                {item.text} {" "}
-                <button onClick={() => handleEdit(item.id)}>Edit</button>{" "}{" "}
-                <button onClick={() => handleDelete(item.id)}>Delete</button>
+                <Card className={classes.root} style={{ backgroundColor: "#e85a50" }}>
+                  <Grid container spacing={3}>
+                    <Grid item xs={7}>
+                      <Paper className={classes.paper}>
+                        {item.text}
+                      </Paper>
+                    </Grid>
+                    <Grid item xs={2.5}>
+                      <Button className={classes.button} style={{ backgroundColor: "#d8c3a4" }} variant="contained" onClick={() => handleEdit(item.id)}>
+                        &nbsp;&nbsp; EDIT &nbsp;&nbsp;
+                      </Button>
+                    </Grid>
+                    <Grid item xs={2.5}>
+                      <Button className={classes.button} style={{ backgroundColor: "#5b5454" }} variant="contained" onClick={() => handleDelete(item.id)}>
+                        DELETE
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Card>
               </div>
               )
           })}
--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,56 @@
 import { useState } from "react";
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: 700,
+        minHeight: 55,
+        marginBottom: 10,
+        marginTop: 10,
+        marginRight: 10
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 20,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+    button: {
+        marginLeft: 7,
+        marginTop: 10,
+    },
+    container: {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(12, 1fr)',
+        gridGap: theme.spacing(3),
+    },
+    paper: {
+        marginTop: 7,
+        marginLeft: 10,
+        fontSize: 20,
+        padding: theme.spacing(1),
+        backgroundColor: "#cfe6ff",
+    },
+
+}));
+
 const FormInput = ({
     item,
     editingIndex, setEditingIndex,
     todos, setTodos
 }) => {
+
+    const classes = useStyles();
 
     const [value, setValue] = useState(item.text);
 
@@ -30,19 +76,33 @@ const FormInput = ({
 
     return (
         <form onSubmit={handelEditFormSubmit} key={item.id}>
-            <label>Edit todo</label>
-            {" "}
-            <input
-                type="text"
-                name="editTodo"
-                placeholder="Edit Todo"
-                defaultValue={item.text}
-                value={value.text}
-                onChange={handelEditInputChange}
-            />
-            {" "}
-            <button type="submit">Update</button> {" "}
-            <button onClick={() => setFalse(item.id)}>Cancel</button>
+            <Card className={classes.root} style={{ backgroundColor: "#F48880" }}>
+                <Grid container spacing={3}>
+                    <Grid item xs={7}>
+
+                        <input
+                            className="editInput"
+                            type="text"
+                            name="editTodo"
+                            placeholder="Edit Todo"
+                            defaultValue={item.text}
+                            value={value.text}
+                            onChange={handelEditInputChange}
+                        />
+
+                    </Grid>
+                    <Grid item xs={2.5}>
+                        <Button className={classes.button} style={{ backgroundColor: "#eae8dc" }} variant="contained" type="submit">
+                            &nbsp;&nbsp; UPDATE &nbsp;&nbsp;
+                        </Button>
+                    </Grid>
+                    <Grid item xs={2.5}>
+                        <Button className={classes.button} style={{ backgroundColor: "#8e8d89" }} variant="contained" onClick={() => setFalse(item.id)}>
+                            CANCEL
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Card>
         </form>
     )
 }
